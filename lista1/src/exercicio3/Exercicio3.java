577086d2c9ec61fa98312286603b4eaea7c35f89
João Gabriel <joogps@gmail.com>
2023-03-07 08:36:13 -0300

    Adicionar exercício 3

---
+++ lista1/src/exercicio3/Exercicio3.java
@@ -0,0 +1,31 @@
+package exercicio3;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		// Double[] lista = new Double[3];
+		Double total = 0.0;
+		
+		for (int i = 0; i < 3; i++) {
+			String valor = JOptionPane.showInputDialog("Digite uma nota:");
+			total+= Double.valueOf(valor);
+		}
+		
+		Double media = total/3.0;
+		String texto = "";
+		if (media >= 6) {
+			texto = "Aprovado(a)";
+		} else if (media >= 4) {
+			texto = "Recuperação";
+		} else {
+			texto = "Reprovado(a)";
+		}
+		
+		JOptionPane.showMessageDialog(null, texto);
+	}
+
+}
